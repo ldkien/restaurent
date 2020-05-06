95e6d33bc88c97461bad80205623a3c51f09e859
Le Doan Kien <kien.le@mservice.com.vn>
2020-05-06 11:27:22 +0700

    [feature-load-balancing] + init load balancing

--- backend-login/main.go
+++ backend-login/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"google.golang.org/grpc"
 	"net"
+	"restaurant/backend-base/app"
 	log "restaurant/backend-base/logger"
 	pb "restaurant/backend-entity/entities"
 	"restaurant/backend-login/grpc/service"
@@ -17,8 +18,8 @@ type server struct {
 	pb.UnimplementedGreeterServer
 }
 
-func main()  {
-	lis, err := net.Listen("tcp", port)
+func main() {
+	lis, err := net.Listen("tcp", app.GlobalConfig.LoginPort)
 	if err != nil {
 		log.Logger.Error("failed to listen: ", err)
 	}
@@ -27,12 +28,9 @@ func main()  {
 	if err := s.Serve(lis); err != nil {
 		log.Logger.Error("failed to serve: ", err)
 	}
+	log.Logger.Info("Start module login port: " + app.GlobalConfig.LoginPort)
 }
 
 func (s *server) Login(ctx context.Context, in *pb.LoginRequest) (*pb.LoginResponse, error) {
 	return service.Login(in)
 }
-
-
-
-
