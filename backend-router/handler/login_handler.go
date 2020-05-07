cd5f5726ca459617cd1b9789971d6e51fa46c829
Le Doan Kien <kien.le@mservice.com.vn>
2020-05-07 16:41:24 +0700

    [feature-optimal-connect-module] + change login with method go

--- backend-router/handler/login_handler.go
+++ backend-router/handler/login_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 	"restaurant/backend-base/entity"
 	backendError "restaurant/backend-base/error"
@@ -13,16 +14,22 @@ import (
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	result := make(chan []byte)
-	go connectLoginModule(result)
+	var loginRequest pb.LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&loginRequest)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	go connectLoginModule(loginRequest, result)
 	w.Write(<-result)
 
 }
 
-func connectLoginModule(result chan []byte) {
+func connectLoginModule(loginRequest pb.LoginRequest, result chan []byte) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer close(result)
 	defer cancel()
-	response, err := client.Client.Login(ctx, &pb.LoginRequest{Username: "kienmit", Password: "63ce66a3f0fd389fb9124826d6cdff29"})
+	response, err := client.Client.Login(ctx, &loginRequest)
 	if err != nil {
 		log.Logger.Error("could not greet: ", err)
 		error := backendError.GetError(backendError.CONNECTOR_ERROR)
